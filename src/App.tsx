40fba389378e7c6fd98acaddca3c45f43cfa6d52
Peterdinis <peterdinis0@pobox.sk>
2024-02-04 18:39:30 +0100

    feat: Add components to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import AboutMe from './components/AboutMe';
+import Expirience from './components/Expirience';
+import Projects from './components/Projects';
 
 const App: FC = () => {
     return (
         <>
             <Navbar />
             <Hero />
-
+            <AboutMe />
+            <Expirience />
+            <Projects />
             <Contact />
             <Footer />
         </>
