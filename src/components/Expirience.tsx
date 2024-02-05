7135f2139e3cb9e1441b8b5946eda014bb11acdd
Peterdinis <peterdinis0@pobox.sk>
2024-02-05 17:41:20 +0100

    feat: Working on design WIP

--- src/components/Expirience.tsx
+++ src/components/Expirience.tsx
@@ -1,17 +1,14 @@
 import { FC } from 'react';
 import SecondaryHeader from './shared/SecondaryHeader';
 import {
-    Box,
-    chakra,
     Container,
-    Text,
-    HStack,
-    VStack,
     Flex,
-    useColorModeValue,
     useBreakpointValue,
+    chakra,
 } from '@chakra-ui/react';
 import EmptyCard from './expirience/EmptyCard';
+import LineDot from './expirience/LineDot';
+import ExpirienceCard from './expirience/ExpirienceCard';
 
 const milestones = [
     {
@@ -40,7 +37,7 @@ const Expirience: FC = () => {
     const isDesktop = useBreakpointValue({ base: false, md: true });
 
     return (
-        <>
+        <chakra.div>
             <SecondaryHeader text='Moje skúsenosti' />
             <Container maxWidth='7xl' p={{ base: 2, sm: 10 }}>
                 {milestones.map((milestone) => (
@@ -48,133 +45,30 @@ const Expirience: FC = () => {
                         {isDesktop && milestone.id % 2 === 0 && (
                             <>
                                 <EmptyCard />
-                                <LineWithDot />
-                                <Card {...milestone} />
+                                <LineDot />
+                                <ExpirienceCard {...milestone} />
                             </>
                         )}
 
                         {isMobile && (
                             <>
-                                <LineWithDot />
-                                <Card {...milestone} />
+                                <LineDot />
+                                <ExpirienceCard {...milestone} />
                             </>
                         )}
 
                         {isDesktop && milestone.id % 2 !== 0 && (
                             <>
-                                <Card {...milestone} />
+                                <ExpirienceCard {...milestone} />
 
-                                <LineWithDot />
+                                <LineDot />
                                 <EmptyCard />
                             </>
                         )}
                     </Flex>
                 ))}
             </Container>
-        </>
-    );
-};
-
-interface CardProps {
-    id: number;
-    title: string;
-    description: string;
-    date: string;
-}
-
-const Card = ({ id, title, description, date }: CardProps) => {
-    // For even id show card on left side
-    // For odd id show card on right side
-    const isEvenId = id % 2 == 0;
-    let borderWidthValue = isEvenId ? '15px 15px 15px 0' : '15px 0 15px 15px';
-    let leftValue = isEvenId ? '-15px' : 'unset';
-    let rightValue = isEvenId ? 'unset' : '-15px';
-
-    const isMobile = useBreakpointValue({ base: true, md: false });
-    if (isMobile) {
-        leftValue = '-15px';
-        rightValue = 'unset';
-        borderWidthValue = '15px 15px 15px 0';
-    }
-
-    return (
-        <HStack
-            flex={1}
-            p={{ base: 3, sm: 6 }}
-            bg={useColorModeValue('gray.100', 'gray.800')}
-            spacing={5}
-            rounded='lg'
-            alignItems='center'
-            pos='relative'
-            _before={{
-                content: `""`,
-                w: '0',
-                h: '0',
-                borderColor: `transparent ${useColorModeValue('#edf2f6', '#1a202c')} transparent`,
-                borderStyle: 'solid',
-                borderWidth: borderWidthValue,
-                position: 'absolute',
-                left: leftValue,
-                right: rightValue,
-                display: 'block',
-            }}
-        >
-            <Box>
-                <Text fontSize='lg' color={isEvenId ? 'teal.400' : 'blue.400'}>
-                    {date}
-                </Text>
-
-                <VStack spacing={2} mb={3} textAlign='left'>
-                    <chakra.h1
-                        fontSize='2xl'
-                        lineHeight={1.2}
-                        fontWeight='bold'
-                        w='100%'
-                    >
-                        {title}
-                    </chakra.h1>
-                    <Text fontSize='md'>{description}</Text>
-                </VStack>
-            </Box>
-        </HStack>
-    );
-};
-
-const LineWithDot = () => {
-    return (
-        <Flex
-            pos='relative'
-            alignItems='center'
-            mr={{ base: '40px', md: '40px' }}
-            ml={{ base: '0', md: '40px' }}
-        >
-            <chakra.span
-                position='absolute'
-                left='50%'
-                height='calc(100% + 10px)'
-                border='1px solid'
-                borderColor={useColorModeValue('gray.200', 'gray.700')}
-                top='0px'
-            ></chakra.span>
-            <Box pos='relative' p='10px'>
-                <Box
-                    pos='absolute'
-                    top='0'
-                    left='0'
-                    bottom='0'
-                    right='0'
-                    width='100%'
-                    height='100%'
-                    backgroundSize='cover'
-                    backgroundRepeat='no-repeat'
-                    backgroundPosition='center center'
-                    bg={useColorModeValue('gray.600', 'gray.200')}
-                    borderRadius='100px'
-                    backgroundImage='none'
-                    opacity={1}
-                ></Box>
-            </Box>
-        </Flex>
+        </chakra.div>
     );
 };
 
---
+++ src/components/expirience/ExpirienceCard.tsx
@@ -0,0 +1,67 @@
+import { useBreakpointValue, HStack, Box, Text, useColorModeValue, VStack, chakra } from "@chakra-ui/react";
+import { FC } from "react";
+
+interface CardProps {
+    id: number;
+    title: string;
+    description: string;
+    date: string;
+}
+
+const ExpirienceCard: FC<CardProps> = ({id, title, description, date}: CardProps) =>{
+    const isEvenId = id % 2 == 0;
+    let borderWidthValue = isEvenId ? '15px 15px 15px 0' : '15px 0 15px 15px';
+    let leftValue = isEvenId ? '-15px' : 'unset';
+    let rightValue = isEvenId ? 'unset' : '-15px';
+
+    const isMobile = useBreakpointValue({ base: true, md: false });
+    if (isMobile) {
+        leftValue = '-15px';
+        rightValue = 'unset';
+        borderWidthValue = '15px 15px 15px 0';
+    }
+
+    return (
+        <HStack
+            flex={1}
+            p={{ base: 3, sm: 6 }}
+            bg={useColorModeValue('gray.100', 'gray.800')}
+            spacing={5}
+            rounded='lg'
+            alignItems='center'
+            pos='relative'
+            _before={{
+                content: `""`,
+                w: '0',
+                h: '0',
+                borderColor: `transparent ${useColorModeValue('#edf2f6', '#1a202c')} transparent`,
+                borderStyle: 'solid',
+                borderWidth: borderWidthValue,
+                position: 'absolute',
+                left: leftValue,
+                right: rightValue,
+                display: 'block',
+            }}
+        >
+            <Box>
+                <Text fontSize='lg' color={isEvenId ? 'teal.400' : 'blue.400'}>
+                    {date}
+                </Text>
+
+                <VStack spacing={2} mb={3} textAlign='left'>
+                    <chakra.h1
+                        fontSize='2xl'
+                        lineHeight={1.2}
+                        fontWeight='bold'
+                        w='100%'
+                    >
+                        {title}
+                    </chakra.h1>
+                    <Text fontSize='md'>{description}</Text>
+                </VStack>
+            </Box>
+        </HStack>
+    );
+}
+
+export default ExpirienceCard;
