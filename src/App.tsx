1eee0791c0b39340c041da39762746be5730b8f4
Peterdinis <peterdinis0@pobox.sk>
2024-02-07 16:34:20 +0100

    FOO

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,20 @@ import FavoriteStack from './components/shared/FavoriteStack';
 import Projects from './components/projects/Projects';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import Layout from './components/shared/Layout';
 
 const App: FC = () => {
     return (
         <>
             <Navbar />
-            <Hero />
-            <AboutMe />
-            <FavoriteStack />
-            <Projects />
-            <Contact />
-            <Footer />
+            <Layout>
+                <Hero />
+                <AboutMe />
+                <FavoriteStack />
+                <Projects />
+                <Contact />
+                <Footer />
+            </Layout>
         </>
     );
 };
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,4 @@
 import {
-    Box,
     Button,
     Flex,
     Heading,
@@ -12,12 +11,7 @@ import { FC } from 'react';
 
 const Hero: FC = () => {
     return (
-        <Box
-            px={{ base: '6', md: '6', lg: '20', sm: '10', xl: '28' }}
-            pb='0'
-            mx='auto'
-            maxW='1536'
-            pt={{ base: '8', sm: '16', md: '20' }}
+        <
         >
             <VStack
                 m='auto'
@@ -116,7 +110,7 @@ const Hero: FC = () => {
                     <Text color="white">abaaa</Text>
                 </Stack>
             </Flex>
-        </Box>
+        </>
     );
 };
 
