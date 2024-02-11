6c950012b5cd6f4e478a17cebf8e6881d5993a29
Peter Dinis <peterdinis0@pobox.sk>
2024-02-11 09:09:06 +0100

    feat: Working on timeline WIP

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -2,35 +2,12 @@ import {
     Box,
     chakra,
     Container,
-    Text,
-    HStack,
-    VStack,
     Flex,
     useColorModeValue,
     useBreakpointValue,
 } from '@chakra-ui/react';
-
-const milestones = [
-    {
-        id: 1,
-        date: 'MARCH 30, 2022',
-        title: 'Chakra Hackathon',
-        description: `Winner of first ever ChakraUI Hackathon. On sait depuis longtemps que travailler avec du texte lisible et contenant du sens.`,
-    },
-    {
-        id: 2,
-        date: 'July 30, 2021',
-        title: 'Open Source, first contribution',
-        description: `Fixing a typo, to fix a bug, contributing to Open Source and collaborating to improve technology for everyone, Ahmad's world changed again!.`,
-    },
-    {
-        id: 3,
-        date: 'July 30, 2018',
-        title: 'Freelancing, started working for myself',
-        description:
-            'Ahmad starts his own business consulting for companies as a fullstack developer. Clients include UK Government departments, UK banks, global fintechs and startups.',
-    },
-];
+import { myExpirience } from './timeline/timeline';
+import TimelineCard from './timeline/TimelineCard';
 
 export const TimeLine = () => {
     const isMobile = useBreakpointValue({ base: true, md: false });
@@ -44,31 +21,27 @@ export const TimeLine = () => {
                 mb={18}
                 textAlign='center'
             >
-                Milestones
+               Moje skúsenosti
             </chakra.h3>
-            {milestones.map((milestone) => (
-                <Flex key={milestone.id} mb='10px'>
-                    {/* Desktop view(left card) */}
-                    {isDesktop && milestone.id % 2 === 0 && (
+            {myExpirience.map((expirience) => (
+                <Flex key={expirience.id} mb='10px'>
+                    {isDesktop && expirience.id % 2 === 0 && (
                         <>
                             <EmptyCard />
                             <LineWithDot />
-                            <Card {...milestone} />
+                            <TimelineCard {...expirience} />
                         </>
                     )}
-
-                    {/* Mobile view */}
                     {isMobile && (
                         <>
                             <LineWithDot />
-                            <Card {...milestone} />
+                            <TimelineCard {...expirience} />
                         </>
                     )}
 
-                    {/* Desktop view(right card) */}
-                    {isDesktop && milestone.id % 2 !== 0 && (
+                    {isDesktop && expirience.id % 2 !== 0 && (
                         <>
-                            <Card {...milestone} />
+                            <TimelineCard {...expirience} />
 
                             <LineWithDot />
                             <EmptyCard />
@@ -77,71 +50,6 @@ export const TimeLine = () => {
                 </Flex>
             ))}
         </Container>
-    );
-};
-
-interface CardProps {
-    id: number;
-    title: string;
-    description: string;
-    date: string;
-}
-
-const Card = ({ id, title, description, date }: CardProps) => {
-    // For even id show card on left side
-    // For odd id show card on right side
-    const isEvenId = id % 2 == 0;
-    let borderWidthValue = isEvenId ? '15px 15px 15px 0' : '15px 0 15px 15px';
-    let leftValue = isEvenId ? '-15px' : 'unset';
-    let rightValue = isEvenId ? 'unset' : '-15px';
-
-    const isMobile = useBreakpointValue({ base: true, md: false });
-    if (isMobile) {
-        leftValue = '-15px';
-        rightValue = 'unset';
-        borderWidthValue = '15px 15px 15px 0';
-    }
-
-    return (
-        <HStack
-            flex={1}
-            p={{ base: 3, sm: 6 }}
-            bg={useColorModeValue('gray.100', 'gray.800')}
-            spacing={5}
-            rounded='lg'
-            alignItems='center'
-            pos='relative'
-            _before={{
-                content: `""`,
-                w: '0',
-                h: '0',
-                borderColor: `transparent ${useColorModeValue('#edf2f6', '#1a202c')} transparent`,
-                borderStyle: 'solid',
-                borderWidth: borderWidthValue,
-                position: 'absolute',
-                left: leftValue,
-                right: rightValue,
-                display: 'block',
-            }}
-        >
-            <Box>
-                <Text fontSize='lg' color={isEvenId ? 'teal.400' : 'blue.400'}>
-                    {date}
-                </Text>
-
-                <VStack spacing={2} mb={3} textAlign='left'>
-                    <chakra.h1
-                        fontSize='2xl'
-                        lineHeight={1.2}
-                        fontWeight='bold'
-                        w='100%'
-                    >
-                        {title}
-                    </chakra.h1>
-                    <Text fontSize='md'>{description}</Text>
-                </VStack>
-            </Box>
-        </HStack>
     );
 };
 
