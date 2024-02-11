cc8ace22d3e53365c5a75882286ec8fbf46e38e5
Peter Dinis <peterdinis0@pobox.sk>
2024-02-11 14:56:04 +0100

    feat: Layout component update

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,6 +1,7 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, Suspense } from 'react';
 import { Box } from '@chakra-ui/react';
 import AnimationWrapper from './AnimationWrapper';
+import FallbackLoader from './FallbackLoader';
 
 interface ILayoutProps {
     children?: ReactNode;
@@ -8,15 +9,17 @@ interface ILayoutProps {
 
 const Layout: FC<ILayoutProps> = ({ children }: ILayoutProps) => {
     return (
-        <Box
-            px={{ base: '6', md: '6', lg: '20', sm: '10', xl: '28' }}
-            pb='0'
-            mx='auto'
-            maxW='1536'
-            pt={{ base: '8', sm: '16', md: '20' }}
-        >
-            <AnimationWrapper>{children}</AnimationWrapper>
-        </Box>
+        <Suspense fallback={<FallbackLoader />}>
+            <Box
+                px={{ base: '6', md: '6', lg: '20', sm: '10', xl: '28' }}
+                pb='0'
+                mx='auto'
+                maxW='1536'
+                pt={{ base: '8', sm: '16', md: '20' }}
+            >
+                <AnimationWrapper>{children}</AnimationWrapper>
+            </Box>
+        </Suspense>
     );
 };
 
