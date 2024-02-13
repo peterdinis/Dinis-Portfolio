aa5b2c67027b5b6a5a267b6555114aa91ab5896f
Peter Dinis <peterdinis0@pobox.sk>
2024-02-13 19:00:54 +0100

    FOO

--- src/components/timeline/ExpirencieTimeline.tsx
+++ src/components/timeline/ExpirencieTimeline.tsx
@@ -1,24 +1,53 @@
+import { Container, chakra, Box, Text } from '@chakra-ui/react';
+import { CheckCircleIcon } from '@chakra-ui/icons';
 import {
-    chakra,
-    Container,
-} from '@chakra-ui/react';
-import { FC } from 'react';
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import { useTranslation } from 'react-i18next';
+import { FC } from 'react';
 
-const ExpirienceTimeline: FC = () => {
-    const { t } = useTranslation();
-    return (
-        <Container id='expirience' maxWidth='7xl' p={{ base: 2, sm: 10 }}>
-            <chakra.h3
-                fontSize='4xl'
-                fontWeight='bold'
-                mb={18}
-                textAlign='center'
-            >
-                {t('description.expirienceHeader')}
-            </chakra.h3>
-        </Container>
-    );
+const ExperienceTimeline: FC = () => {
+  const { t } = useTranslation();
+
+  const timelineElements = [
+    { text: 'Eat', icon: <CheckCircleIcon color='blue.500' boxSize={6} /> },
+    { text: 'Eat', icon: <CheckCircleIcon color='blue.500' boxSize={6} /> },
+    { text: 'Eat', icon: <CheckCircleIcon color='blue.500' boxSize={6} /> },
+    { text: 'Eat', icon: <CheckCircleIcon color='blue.500' boxSize={6} /> },
+    { text: 'Eat', icon: <CheckCircleIcon color='blue.500' boxSize={6} /> },
+  ];
+
+  return (
+    <Container id='experience' maxWidth='7xl' p={{ base: 2, sm: 10 }}>
+      <chakra.h3 fontSize='4xl' fontWeight='bold' mb={18} textAlign='center'>
+        {t('description.experienceHeader')}
+      </chakra.h3>
+
+      <VerticalTimeline layout="1-column">
+        {timelineElements.map((element, index) => (
+          <VerticalTimelineElement
+            key={index}
+            contentStyle={{
+              background: '#fff',
+              boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+              borderRadius: '8px',
+              padding: '16px',
+            }}
+            contentArrowStyle={{
+              borderRight: '7px solid #fff',
+            }}
+            position={index % 2 === 0 ? 'left' : 'right'} // Alternate between left and right
+          >
+            <Box className='timelineContentContainer' p={4}>
+              <Text>{element.text}</Text>
+            </Box>
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+    </Container>
+  );
 };
 
-export default ExpirienceTimeline;
+export default ExperienceTimeline;
--- src/theme.ts
+++ src/theme.ts
@@ -1,25 +1,22 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
     styles: {
-      global: {
-        '.timeline': {
-          transform: 'rotate(90deg)',
+        global: {
+            '.timelineContentContainer': {
+                textAlign: 'left',
+            },
+            '.timelineContent': {
+                display: 'inline-block',
+                transform: 'rotate(-90deg)',
+                textAlign: 'center',
+                minWidth: 50,
+            },
+            '.timelineIcon': {
+                transform: 'rotate(-90deg)',
+            },
         },
-        '.timelineContentContainer': {
-          textAlign: 'left',
-        },
-        '.timelineContent': {
-          display: 'inline-block',
-          transform: 'rotate(-90deg)',
-          textAlign: 'center',
-          minWidth: 50,
-        },
-        '.timelineIcon': {
-          transform: 'rotate(-90deg)',
-        },
-      },
     },
-  });
+});
 
-export default theme
+export default theme;
