b532957e3e5a0e88edb971a020fbe58a2fbf86d7
Peter Dinis <peterdinis0@pobox.sk>
2024-02-13 16:39:28 +0100

    refactor: Start to refactoring timeline component WIP

--- src/components/timeline/ExpirencieTimeline.tsx
+++ src/components/timeline/ExpirencieTimeline.tsx
@@ -1,16 +1,9 @@
-import { chakra, Container, Flex, useBreakpointValue } from '@chakra-ui/react';
-import { myExpirience } from './timeline';
-import TimelineCard from './TimelineCard';
-import EmptyTimelineCard from './EmptyTimelineCard';
-import TimelineDot from './TimelineDot';
+import { chakra, Container} from '@chakra-ui/react';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const ExpirienceTimeline: FC = () => {
-    const isMobile = useBreakpointValue({ base: true, md: false });
-    const isDesktop = useBreakpointValue({ base: false, md: true });
     const { t } = useTranslation();
-
     return (
         <Container id='expirience' maxWidth='7xl' p={{ base: 2, sm: 10 }}>
             <chakra.h3
@@ -21,32 +14,7 @@ const ExpirienceTimeline: FC = () => {
             >
                 {t('description.expirienceHeader')}
             </chakra.h3>
-            {myExpirience.map((expirience) => (
-                <Flex key={expirience.id} mb='10px'>
-                    {isDesktop && expirience.id % 2 === 0 && (
-                        <>
-                            <EmptyTimelineCard />
-                            <TimelineDot />
-                            <TimelineCard {...expirience} />
-                        </>
-                    )}
-                    {isMobile && (
-                        <>
-                            <TimelineDot />
-                            <TimelineCard {...expirience} />
-                        </>
-                    )}
-
-                    {isDesktop && expirience.id % 2 !== 0 && (
-                        <>
-                            <TimelineCard {...expirience} />
-
-                            <TimelineDot />
-                            <EmptyTimelineCard />
-                        </>
-                    )}
-                </Flex>
-            ))}
+            
         </Container>
     );
 };
