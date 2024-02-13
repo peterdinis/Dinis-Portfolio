2730c8a7c354fff1e50842ac5163e616fb806e47
Peter Dinis <peterdinis0@pobox.sk>
2024-02-13 19:08:34 +0100

    feat: Working on new timeline design

--- src/components/timeline/ExpirencieTimeline.tsx
+++ src/components/timeline/ExpirencieTimeline.tsx
@@ -1,4 +1,4 @@
-import { Container, chakra, Box, Text } from '@chakra-ui/react';
+import { Container, chakra, Box, Text, useColorMode } from '@chakra-ui/react';
 import { CheckCircleIcon } from '@chakra-ui/icons';
 import {
   VerticalTimeline,
@@ -7,9 +7,11 @@ import {
 import 'react-vertical-timeline-component/style.min.css';
 import { useTranslation } from 'react-i18next';
 import { FC } from 'react';
+import { motion } from 'framer-motion';
 
 const ExperienceTimeline: FC = () => {
   const { t } = useTranslation();
+  const { colorMode } = useColorMode();
 
   const timelineElements = [
     { text: 'Eat', icon: <CheckCircleIcon color='blue.500' boxSize={6} /> },
@@ -20,30 +22,42 @@ const ExperienceTimeline: FC = () => {
   ];
 
   return (
-    <Container id='experience' maxWidth='7xl' p={{ base: 2, sm: 10 }}>
+    <Container
+      id='experience'
+      maxWidth='7xl'
+      p={{ base: 2, sm: 10 }}
+      color={colorMode === 'dark' ? 'white' : 'inherit'}
+    >
       <chakra.h3 fontSize='4xl' fontWeight='bold' mb={18} textAlign='center'>
         {t('description.experienceHeader')}
       </chakra.h3>
 
       <VerticalTimeline layout="1-column">
         {timelineElements.map((element, index) => (
-          <VerticalTimelineElement
+          <motion.div
             key={index}
-            contentStyle={{
-              background: '#fff',
-              boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
-              borderRadius: '8px',
-              padding: '16px',
-            }}
-            contentArrowStyle={{
-              borderRight: '7px solid #fff',
-            }}
-            position={index % 2 === 0 ? 'left' : 'right'} // Alternate between left and right
+            whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
           >
-            <Box className='timelineContentContainer' p={4}>
-              <Text>{element.text}</Text>
-            </Box>
-          </VerticalTimelineElement>
+            <VerticalTimelineElement
+              icon={element.icon}
+              contentStyle={{
+                background: colorMode === 'dark' ? '#2D3748' : '#fff',
+                boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+                borderRadius: '8px',
+                padding: '16px',
+                marginTop: "2rem",
+                color: colorMode === 'dark' ? 'white' : 'inherit',
+              }}
+              contentArrowStyle={{
+                borderRight: `7px solid ${colorMode === 'dark' ? '#2D3748' : '#fff'}`,
+              }}
+              position={index % 2 === 0 ? 'left' : 'right'}
+            >
+              <Box className='timelineContentContainer' p={4}>
+                <Text>{element.text}</Text>
+              </Box>
+            </VerticalTimelineElement>
+          </motion.div>
         ))}
       </VerticalTimeline>
     </Container>
