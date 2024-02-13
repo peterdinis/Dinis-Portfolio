62a4e67483070ff7459bb29ae70e8cd485f0b3a6
Peter Dinis <peterdinis0@pobox.sk>
2024-02-13 16:18:58 +0100

    feat: Adding missing translation to i18n

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -39,6 +39,16 @@ i18n.use(LanguageDetector)
             sk: {
                 translation: {
                     description: {
+                        firstExpirienceH: "SPŠT Bardejov (1-rok) práca popri škole. FullStack Developer",
+                        firstExpirienceSubH: "Školská knižnica projekt Unicorn React-Node-MongoDB",
+                        firstExpirienceD: "Školská súťaž ktorú organizovala firma Unicorn ktorá má pobočku v Prešove. Zapojila sa do nej aj moja stredná škola ktorú som navštevoval. Fungovalo to na princípe toho, že škola si vybrala tému a pridelený pracovník z firmy unicorn zo žiakmi danú applikáciu vytváral.Frontend sa robil v Reacte, backend Node.js a MongoDB(databáza)(Firma Unicorn má používa upravenú verziu reactu a node a mongo). Výsledok takmer ročnej práce bol taký, že sa vytvorila školská knižnica, kde žiak si vie požičať knihu vrátiť ju. Učiteľ vie robiť CRUD operácie pre knihy a kategórie a admin(správca) vie dať permisie učiteľom(spravovať knižnicu). Applikáciu som vytvoril sám",
+                        secondExpirienceH: "Think Easy (3-Mesiace) Frontend Developer",
+                        secondExpirienceD: "V tejto firme som bol počas VŠ. Technológie React Typescript Redux(neskôr React-Query). Pracoval som na applikácií pre Domov Dôchodcov(applikácia na volala Legato). Applikácia ktorá pravdepobodne je vo vývoji stále, fungovala tak že klienti mali na ruke náramky a v tej applikácií zamestanec mohol vidieť či klient je na izbe, alebo je u rodiny, alebo je u lekára). Bol som v malom 10-členom týme, ktorí vytváral tú applikáciu",
+                        thirdExpirienceH: "Navisys 11 Mesiacov",
+                        thirdExpirienceD: "Firma vytvárala eshop na ktorom sa dajú predávať licencie na softverové produkty. Vedel som byť predajca alebo výrobca. V applikácií som si vedel vytvárať rôzne vzťahy(predajca - výrobca). Techonológie ktoré boli použité: Nx(Monorepo) Vuejs, Nestjs, Docker, Postgresql, Jest. Bol som v malom 7-členom týme ktorý vytváral tú applikáciu.",
+                        fourExpirienceH: "Jump Soft Od 1.3.2023",
+                        fourExpirinceSubH: "Full stack developer",
+                        fourExpirinceD: "Interná vývojová platforma a jazyk spoločnosti (Twinzu), Java, Javascript, PostgreSQL, GitLab, Docker. Projekty, na ktorých pracujem/pracoval som: 1. Slovenská komora daňových poradcov. 2. Úrad pre dohľad nad zdravotnou starostlivosťou UDZS",
                         expirienceHeader: "Moje skúsenosti",
                         secondProjectH: "Spšt knižnica API",
                         sevenProjectD: "Reddit klon",
