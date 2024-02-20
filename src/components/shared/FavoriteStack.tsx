c6cda5218d07f1967fc466ff25ed331e4ab2f6b8
Peter Dinis <peterdinis0@pobox.sk>
2024-02-20 18:02:26 +0100

    feat: Update favorite stack

--- src/components/shared/FavoriteStack.tsx
+++ src/components/shared/FavoriteStack.tsx
@@ -21,6 +21,9 @@ import figma from "../../images/figma-new.png";
 import { useTranslation } from 'react-i18next';
 import linux from "../../images/linux-new.png";
 import mongo from "../../images/mongo.png";
+import csharp from "../../images/csharp.png";
+import tailwind from "../../images/tailwind-new.png";
+import zustand from "../../images/zustand.png";
 
 const FavoriteStack: FC = () => {
     const { colorMode } = useColorMode();
@@ -57,13 +60,22 @@ const FavoriteStack: FC = () => {
                         <Image m={10} src={graphql} w={20} boxShadow={12} h={20} />
                     </motion.div>
                     <motion.div variants={imageVariants} whileHover='hover'>
+                        <Image m={10} src={csharp} w={20} boxShadow={12} h={20} />
+                    </motion.div>
+                    <motion.div variants={imageVariants} whileHover='hover'>
                         <Image m={10} src={figma} w={20} boxShadow={12} h={20} />
+                    </motion.div>
+                    <motion.div variants={imageVariants} whileHover='hover'>
+                        <Image m={10} src={tailwind} w={20} boxShadow={12} h={20} />
                     </motion.div>
                     <motion.div variants={imageVariants} whileHover='hover'>
                         <Image m={10} src={mongo} w={20} boxShadow={12} h={20} />
                     </motion.div>
                     <motion.div variants={imageVariants} whileHover='hover'>
                         <Image m={10} src={linux} w={20} boxShadow={12} h={20} />
+                    </motion.div>
+                    <motion.div variants={imageVariants} whileHover='hover'>
+                        <Image m={10} src={zustand} w={20} boxShadow={12} h={20} />
                     </motion.div>
                     <motion.div variants={imageVariants} whileHover='hover'>
                         <Image
