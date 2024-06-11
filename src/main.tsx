1c624da8c08a3865e770eae64112c6b08ed4bae4
Peter Dinis <peterdinis0@pobox.sk>
2024-06-11 12:47:47 +0200

    Not Found Page add to portfolio + tanstack/router

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,19 @@ import { ChakraProvider, Spinner } from "@chakra-ui/react";
 import { StrictMode, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "react-vertical-timeline-component/style.min.css";
-import App from "./App.tsx";
 import "./i18n/i18n.ts";
 import "./index.css";
 import ScrollToTop from "./components/ScrollToTop.tsx";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+
+const router = createRouter({routeTree});
+
+declare module '@tanstack/react-router' {
+	interface Register {
+	  router: typeof router;
+	}
+  }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<StrictMode>
@@ -21,7 +30,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 					/>
 				}
 			>
-				<App />
+				<RouterProvider router={router} />
 			</Suspense>
 			<ScrollToTop />
 		</ChakraProvider>
