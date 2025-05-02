d88cfe72223dfe14ebb07daee7a3715c96e49880
peterdinis <petter1@pobox.sk>
2025-05-02 18:35:09 +0200

    feat: Delete not-found component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,5 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router';
-import NotFoundWrapper from '../components/NotFoundWrapper';
 
 export const Route = createRootRoute({
     component: () => <Outlet />,
-    notFoundComponent: () => <NotFoundWrapper />,
 });
