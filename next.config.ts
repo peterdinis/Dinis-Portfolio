e05259e649cc0081a7ede1f07feab5d7a4478f23
peterdinis <petter1@pobox.sk>
2025-11-08 13:35:53 +0100

    feat: Update next-config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     experimental: {
-        esmExternals: true
+        esmExternals: true,
     }
 };
 
